1d09ff20e1962cdce2285f6708e5cd821bdb9ae4
scherepanov <sergey@cherepanov.org.ua>
2021-07-16 19:08:59 +0400

    configs

--- fpm/entrypoint.sh
+++ fpm/entrypoint.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "[$(date +'%F %T')] ==> Restoring shared folders ownership"
-sudo mkdir -p /var/www/public/{cache}
-sudo mkdir -p /var/www/var/{logs,cache,file_storage}
-sudo chown -R www-data:www-data /var/www/public/{cache}
-sudo chown -R www-data:www-data /var/www/var/{logs,cache,file_storage}
-
-echo "[$(date +'%F %T')] ==> Clearing the cache"
-NO_DOCKER=true make cache
+echo "[$(date +'%F %T')] ==> Creating cache dirs"
+mkdir -p /var/www/public/{cache}
+mkdir -p /var/www/var/{logs,cache,file_storage}
 
 cd /var/www
-sudo mkdir -p /var/www/.composer
-sudo chmod 0777 /var/www/.composer
+mkdir -p /var/www/.composer
+chmod 0777 /var/www/.composer
 
+#echo "[$(date +'%F %T')] ==> Clearing the cache"
+#NO_DOCKER=true make cache
 #
 #echo "[$(date +'%F %T')] ==> Installing vendors"
 #composer-1.10 install --optimize-autoloader --no-progress --no-dev --no-interaction
